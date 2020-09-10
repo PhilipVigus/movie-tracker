802fadab2855aa3abc692e26206e180f8ae24d4a
PhilipVigus <philipvigus@yahoo.co.uk>
2020-09-10 14:24:12 +0100

    Add logout button

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
-  const { loginWithRedirect } = useAuth0();
+  const { loginWithRedirect, logout } = useAuth0();
 
   return (
     <div>
       <button onClick={() => loginWithRedirect()}>Log In</button>
+      <button onClick={() => logout({ returnTo: window.location.origin })}>
+        Log Out
+      </button>
     </div>
   );
 }
