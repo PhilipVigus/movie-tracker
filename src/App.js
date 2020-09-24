c8e2f5902789c53135354c4f7ce5d6add12c5d43
PhilipVigus <philipvigus@yahoo.co.uk>
2020-09-24 14:08:55 +0100

    Delete netlify functions spike

--- src/App.js
+++ src/App.js
@@ -1,54 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import Axios from "axios";
 
 function App() {
-  const {
-    loginWithPopup,
-    logout,
-    isAuthenticated,
-    user,
-    getAccessTokenSilently,
-  } = useAuth0();
-  const [response, setResponse] = useState("Not set");
+  const { loginWithPopup, logout, isAuthenticated, user } = useAuth0();
 
-  useEffect(() => {
-    const getUserProfile = async () => {
-      try {
-        const token = await getAccessTokenSilently({
-          audience: process.env.REACT_APP_AUTH0_AUDIENCE,
-        });
-
-        console.log(token);
-        const api = await Axios.get("/.netlify/functions/me", {
-          headers: {
-            authorization: "Bearer " + token,
-          },
-        });
-
-        console.log("here");
-        console.log(api);
-
-        const data = {
-          status: api.status,
-          statusText: api.statusText,
-          body: api,
-        };
-
-        console.log(data);
-        setResponse(data);
-      } catch (e) {
-        setResponse(e.message);
-      }
-    };
-
-    if (isAuthenticated) {
-      console.log("Getting user profile");
-      getUserProfile();
-    }
-  }, [isAuthenticated, getAccessTokenSilently]);
-  console.log(response);
-  console.log(response.body);
   return (
     <div>
       <button onClick={() => loginWithPopup()}>Log In</button>
