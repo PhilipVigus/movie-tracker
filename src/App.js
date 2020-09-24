41e52bcc5176673c6e7813d8fa54b515b9e68e12
PhilipVigus <philipvigus@yahoo.co.uk>
2020-09-24 15:03:49 +0100

    Get api access token

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import React from "react";
-import { Link, Switch, Route, Redirect } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Link, Switch, Route } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 import PrivateComponent from "./components/PrivateComponent";
 
 function App() {
-  const { loginWithPopup, logout, isAuthenticated, user } = useAuth0();
+  const {
+    loginWithPopup,
+    logout,
+    isAuthenticated,
+    user,
+    getAccessTokenSilently,
+  } = useAuth0();
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const token = await getAccessTokenSilently({
+          audience: process.env.REACT_APP_AUTH0_AUDIENCE,
+        });
+
+        console.log(token);
+      } catch (e) {
+        console.error(e);
+      }
+    };
+
+    if (isAuthenticated) {
+      getData();
+    }
+  }, [getAccessTokenSilently, isAuthenticated]);
 
   return (
     <>
