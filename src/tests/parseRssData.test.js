2e81cdea2e5c98b2e97334410c5301fbae24db2c
PhilipVigus <philipvigus@yahoo.co.uk>
2020-10-03 11:24:02 +0100

    Parse date

--- src/tests/parseRssData.test.js
+++ src/tests/parseRssData.test.js
@@ -7,6 +7,7 @@ describe("parseRssData", () => {
       {
         guid: "http://www.traileraddict.com/another-round-druk/trailer",
         title: "Another Round: Trailer",
+        date: "Wed, 23 Sep 2020 10:58:54 -0700",
         link: "http://www.traileraddict.com/another-round-druk/trailer",
         image:
           "http://cdn.traileraddict.com/vidog/studiocanal/druk-poster/1.jpg",
@@ -17,6 +18,7 @@ describe("parseRssData", () => {
         title: "Antebellum: TV Spot - Don't Give Away the Twist",
         guid:
           "http://www.traileraddict.com/antebellum-2020/tv-spot-dont-give-away-the-twist",
+        date: "Wed, 23 Sep 2020 10:47:50 -0700",
         link:
           "http://www.traileraddict.com/antebellum-2020/tv-spot-dont-give-away-the-twist",
         image:
