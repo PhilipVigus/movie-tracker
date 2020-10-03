6740f0f9d6d68f8581ce143c4e84ec19b77d7a5c
PhilipVigus <philipvigus@yahoo.co.uk>
2020-10-03 11:21:48 +0100

    Parse tags

--- src/tests/parseRssData.test.js
+++ src/tests/parseRssData.test.js
@@ -9,7 +9,9 @@ describe("parseRssData", () => {
         title: "Another Round: Trailer",
         link: "http://www.traileraddict.com/another-round-druk/trailer",
         image:
-          "http://cdn.traileraddict.com/vidog/studiocanal/druk-poster/1.jpg"
+          "http://cdn.traileraddict.com/vidog/studiocanal/druk-poster/1.jpg",
+        tags:
+          '<a href="http://www.traileraddict.com/tags/studiocanal">Studio Canal</a>, <a href="http://www.traileraddict.com/tags/drama">Drama</a>, <a href="http://www.traileraddict.com/tags/mads-mikkelsen">Mads Mikkelsen</a>, <a href="http://www.traileraddict.com/tags/thomas-bo-larsen">Thomas Bo Larsen</a>'
       },
       {
         title: "Antebellum: TV Spot - Don't Give Away the Twist",
@@ -18,7 +20,9 @@ describe("parseRssData", () => {
         link:
           "http://www.traileraddict.com/antebellum-2020/tv-spot-dont-give-away-the-twist",
         image:
-          "http://cdn.traileraddict.com/vidog/lionsgate/antebellum-2020-poster/1.jpg"
+          "http://cdn.traileraddict.com/vidog/lionsgate/antebellum-2020-poster/1.jpg",
+        tags:
+          '<a href="http://www.traileraddict.com/tags/lionsgate">Lionsgate</a>, <a href="http://www.traileraddict.com/tags/thriller">Thriller</a>, <a href="http://www.traileraddict.com/tags/tv-spot">TV Spot</a>, <a href="http://www.traileraddict.com/tags/janelle-monae">Janelle MonÃ¡e</a>'
       }
     ];
 
--- utils/parseRssData.js
+++ utils/parseRssData.js
@@ -7,11 +7,13 @@ const parseRssData = (data) => {
 
     trailersAsJson.forEach((trailer) => {
       const imageLink = trailer.description[0].match(/<img src="(.*)" \/>/)[1];
+      const tags = trailer.description[0].match(/Tags: (<a.*\/a>)/)[1];
       trailers.push({
         guid: trailer.guid[0],
         title: trailer.title[0],
         link: trailer.link[0],
         image: imageLink,
+        tags,
       });
     });
   });
