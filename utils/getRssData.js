8b982cf1c42ceda5eec7c88495ff05c451277e6f
PhilipVigus <philipvigus@yahoo.co.uk>
2020-10-03 10:34:21 +0100

    Delete console.logs

--- utils/getRssData.js
+++ utils/getRssData.js
@@ -1,8 +1,6 @@
 const axios = require("axios");
 
 const getRssData = async () => {
-  console.log("there");
-  console.log(process.env.NODE_ENV);
   try {
     const data = await axios.get("https://www.traileraddict.com/rss");
     return data;
