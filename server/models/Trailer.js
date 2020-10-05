5314e1c75db8d76407fedf0ea0418b0366d630b2
PhilipVigus <philipvigus@yahoo.co.uk>
2020-10-05 11:01:49 +0100

    Add unique and required specifiers to trailer model

--- server/models/Trailer.js
+++ server/models/Trailer.js
@@ -3,12 +3,32 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const TrailerSchema = new Schema({
-  guid: String,
-  title: String,
-  date: String,
-  link: String,
-  image: String,
-  tags: String
+  _id: String,
+  title: {
+    type: String,
+    unique: false,
+    required: true
+  },
+  date: {
+    type: String,
+    unique: false,
+    required: true
+  },
+  link: {
+    type: String,
+    unique: false,
+    required: true
+  },
+  image: {
+    type: String,
+    unique: false,
+    required: true
+  },
+  tags: {
+    type: String,
+    unique: false,
+    required: true
+  }
 });
 
 export default TrailerSchema;
